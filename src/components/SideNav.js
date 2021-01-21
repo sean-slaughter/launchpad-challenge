c1eab4606881484f9d6b388142c11558b121d71d
sean-slaughter <sthompson2822@gmail.com>
2021-01-21 12:59:14 -0600

    create sidenav component

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,9 +1,25 @@
+import { Divider, List, ListItemIcon, ListItemText, makeStyles, Drawer, ListItem } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles({
+    sidenav: {
+        width: 170,
+    }
+})
+
 const SideNav = () => {
+    const classes = useStyles();
     return (
-        <div>
-            
+        <div className={classes.sidenav}>
+            <Drawer variant="permanent"  anchor="left">
+                <Divider/>
+                <List>
+                    <ListItem button key="Test Button">
+                        <ListItemIcon>Icon</ListItemIcon>
+                        <ListItemText primary=""/>
+                    </ListItem>
+                    </List> 
+            </Drawer>
         </div>
     );
 }
