4d5cc8b3de79e43392c4965de83c3284bb535ca8
sean-slaughter <sthompson2822@gmail.com>
2021-01-21 14:53:03 -0600

    add redux store and repo reducer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import repoReducer from './reducers/repoReducer';
+
+
+const store = createStore(
+  repoReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : (f) => f
+  )
+);
 
 const theme = createMuiTheme({
   palette: {
@@ -18,11 +31,13 @@ const theme = createMuiTheme({
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <App/>
       </ThemeProvider>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducers/repoReducer.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+    state: "WORKING"
+}
+
+const repoReducer = (state = initialState, action) =>{
+    switch(action.type){
+        default:
+            return state;
+    }
+}
+
+export default repoReducer
