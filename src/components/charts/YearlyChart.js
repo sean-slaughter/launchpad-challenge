d956f0a0b15c8b9ce13f599db5222fc8a1bfad83
sean-slaughter <sthompson2822@gmail.com>
2021-01-22 15:43:58 -0600

    Create skeleton yearlychart component

---
+++ src/components/charts/YearlyChart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Line2D from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+ReactFC.fcRoot(FusionCharts, Line2D, FusionTheme);
+
+const YearlyChart = () => {
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default YearlyChart;
