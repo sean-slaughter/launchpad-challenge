a32c35b65dc3d3b86c748a7cbdca4aab40cea2b8
sean-slaughter <sthompson2822@gmail.com>
2021-01-22 15:44:31 -0600

    Refactor routes

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import reposData from '../data/reposData';
-import Compare from './Compare';
-import Home from './Home';
-import Repo from './Repo';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import reposData from "../data/reposData";
+import Compare from "./Compare";
+import Home from "./Home";
+import Repo from "./Repo";
 
 const Routes = () => {
-    return (
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/compare" component={Compare}/>
-            {reposData.map((repo, index) => {
-                return <Route key={index} path={repo.path} render={() => <Repo repo={repo}/>}/> 
-            })}
-        </Switch>
-    );
-}
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/compare" component={Compare} />
+      {reposData.map((repo, index) => {
+        return (
+          <Route
+            key={index}
+            path={repo.path}
+            render={() => <Repo repo={repo} />}
+          />
+        );
+      })}
+    </Switch>
+  );
+};
 
 export default Routes;
