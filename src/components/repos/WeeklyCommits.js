37888c8fc4ee7e1167b1fe2e6ce9e7bc33757bcf
sean-slaughter <sthompson2822@gmail.com>
2021-01-22 17:13:24 -0600

    create skeleton components for dashboard

---
+++ src/components/repos/WeeklyCommits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WeeklyCommits = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default WeeklyCommits;
---
+++ src/components/repos/YearlyCommits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const YearlyCommits = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default YearlyCommits;
