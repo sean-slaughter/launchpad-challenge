e20119f9ccd7b506311ec4ef4126df17ed8832d3
sean-slaughter <sthompson2822@gmail.com>
2021-01-23 13:03:33 -0600

    Add left padding to main container

--- src/components/containers/MainContainer.js
+++ src/components/containers/MainContainer.js
@@ -1,25 +1,26 @@
-import { Container, Grid, makeStyles } from '@material-ui/core';
-import React from 'react';
-import Routes from '../../Routes';
+import { Container, Grid, makeStyles } from "@material-ui/core";
+import React from "react";
+import Routes from "../../Routes";
 
 const useStyles = makeStyles({
-   root: {
-       backgroundColor: "#eae0d5",
-       width: "100%",
-       height: "100%",
-   },
-})
+  root: {
+    backgroundColor: "#eae0d5",
+    width: "100%",
+    height: "100vh",
+    paddingLeft: "15vw",
+  },
+});
 const MainContainer = () => {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-            <Container>
-                <Grid container direction="row" justify="center" alignItems="center">
-                    <Routes/>
-                </Grid>
-            </Container>
-        </div>
-    );
-}
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Container>
+        <Grid container direction="row" justify="center" alignItems="center">
+          <Routes />
+        </Grid>
+      </Container>
+    </div>
+  );
+};
 
 export default MainContainer;
