5f55dab8d6eb789f5f240398d0ac3b68165a83dd
sean-slaughter <sthompson2822@gmail.com>
2021-01-24 13:53:29 -0600

    refactor chart data

--- src/components/repos/charts/CompareWeeklyChart.js
+++ src/components/repos/charts/CompareWeeklyChart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import ReactFC from "react-fusioncharts";
 import FusionCharts from "fusioncharts";
 import Line2D from "fusioncharts/fusioncharts.charts";
@@ -6,90 +6,53 @@ import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
 ReactFC.fcRoot(FusionCharts, Line2D, FusionTheme);
 
-const CompareWeeklyChart = ( {data} ) => {
-    
-   const getChartData = () => {
-     const chartData = [];
-     Object.entries(data).forEach(([key, value]) => {
-       chartData.push({
-         seriesName: key,
-         data: [
-           {
-             value: value[0].days[0],
-           },
-           {
-             value: value[0].days[1],
-           },
-           {
-             value: value[0].days[2],
-           },
-           {
-             value: value[0].days[3],
-           },
-           {
-             value: value[0].days[4],
-           },
-           {
-             value: value[0].days[5],
-           },
-           {
-             value: value[0].days[6],
-           },
-         ],
-       });
-     });
-     return chartData;
-   };
+const CompareWeeklyChart = ({ data }) => {
 
-   const chartConfigs = {
-     type: "msline",
-     width: "500",
-     height: "300",
-     dataFormat: "json",
-     dataSource: {
-       chart: {
-         baseFont: "Titillium Web",
-         animation: 1,
-         caption: "Number of commits each day over the last week.",
-         xAxisName: "Day",
-         yAxisName: "# of Commits",
-         theme: "fusion",
-       },
-       categories: [
-         {
-           category: [
-             {
-               label: "Sun",
-             },
-             {
-               label: "Mon",
-             },
-             {
-               label: "Tue",
-             },
-             {
-               label: "Wed",
-             },
-             {
-               label: "Thu",
-             },
-             {
-               label: "Fri",
-             },
-             {
-               label: "Sat",
-             },
-             {
-               label: "Sun",
-             },
-           ],
-         },
-       ],
-       dataset: getChartData(),
-     },
-   };
+  const chartData = Object.entries(data).map(([key, value]) => {
+    return {
+      seriesName: key,
+      data: value[0].days.map((day) => {
+        console.log(day);
+        return {
+          value: day,
+        };
+      }),
+    };
+  });
 
-    return <ReactFC {...chartConfigs} />;
-}
+  const chartConfigs = {
+    type: "msline",
+    width: "500",
+    height: "300",
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        baseFont: "Titillium Web",
+        animation: 1,
+        caption: "Number of commits each day over the last week.",
+        xAxisName: "Day",
+        yAxisName: "# of Commits",
+        theme: "fusion",
+      },
+      categories: [
+        {
+          category: [
+            { label: "Sun" },
+            { label: "Mon" },
+            { label: "Tue" },
+            { label: "Wed" },
+            { label: "Thu" },
+            { label: "Fri" },
+            { label: "Sat" },
+            { label: "Sun" },
+          ],
+        },
+      ],
+      dataset: chartData,
+    },
+  };
+
+  return <ReactFC {...chartConfigs}/>;
+};
 
 export default CompareWeeklyChart;
